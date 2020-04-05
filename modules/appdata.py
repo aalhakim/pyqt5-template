82c445c73b4f8ceae04da0b808248d661b6c3100
Ali H Al-Hakim <alihalhakim@hotmail.com>
2020-04-05 11:58:12 +0100

    Create appdata.py
    Contains information about the application.

---
+++ modules/appdata.py
@@ -0,0 +1,16 @@
+"""
+Information about the application.
+"""
+
+########################################################################
+# Application Data
+ORGANISATION_NAME = "Alpaca Apps Ltd"
+APPLICATION_NAME = "PyQt5 Example Application"
+VERSION = "0.1.0"
+RELEASE_DATE = "04 April 2020"
+
+
+########################################################################
+# Dialog Window Names
+DIALOG_NAME_HELP_ABOUT = "Help - About"
+DIALOG_NAME_HELP_GUIDE = "Help - User Guide"
